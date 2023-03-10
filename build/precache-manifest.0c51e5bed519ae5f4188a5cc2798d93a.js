0758af9d3b7a03580bc37d8970c5e3591fa36992
CatherineJOnia <catherinejonia@gmail.com>
2023-03-10 14:59:27 -0500

    build command

---
+++ build/precache-manifest.0c51e5bed519ae5f4188a5cc2798d93a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0978d6b89efc16935c0e29312c64724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c357de9cbd4bbc1bb15",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "439ae9f7e095611709b7",
+    "url": "/static/css/main.76490d47.chunk.css"
+  },
+  {
+    "revision": "8c357de9cbd4bbc1bb15",
+    "url": "/static/js/2.3cdb3411.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/static/js/2.3cdb3411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "439ae9f7e095611709b7",
+    "url": "/static/js/main.e65f48a2.chunk.js"
+  },
+  {
+    "revision": "2a4b0a8b529886aef6fe",
+    "url": "/static/js/runtime-main.3a68f448.js"
+  },
+  {
+    "revision": "8d2294304193b54cf97594aaf2a3966f",
+    "url": "/static/media/dark-background.8d229430.svg"
+  },
+  {
+    "revision": "d05ff2d277c251a018af82d1a84ddc70",
+    "url": "/static/media/light-background.d05ff2d2.svg"
+  }
+]);
