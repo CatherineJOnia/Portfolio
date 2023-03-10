9ad75ee2663ff08c70b60ab6850f77c7d415d6c2
CatherineJOnia <catherinejonia@gmail.com>
2023-03-10 14:27:55 -0500

    removed unnecessary code

---
+++ build/precache-manifest.2ee20d92437437f647b32689b4c3c658.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0185b7e12abcf64ffe71a23cabb2aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c357de9cbd4bbc1bb15",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "6e60dc1deb3d0e4d1bc5",
+    "url": "/static/css/main.76490d47.chunk.css"
+  },
+  {
+    "revision": "8c357de9cbd4bbc1bb15",
+    "url": "/static/js/2.3cdb3411.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/static/js/2.3cdb3411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e60dc1deb3d0e4d1bc5",
+    "url": "/static/js/main.2bfdb07e.chunk.js"
+  },
+  {
+    "revision": "2a4b0a8b529886aef6fe",
+    "url": "/static/js/runtime-main.3a68f448.js"
+  },
+  {
+    "revision": "8d2294304193b54cf97594aaf2a3966f",
+    "url": "/static/media/dark-background.8d229430.svg"
+  },
+  {
+    "revision": "d05ff2d277c251a018af82d1a84ddc70",
+    "url": "/static/media/light-background.d05ff2d2.svg"
+  }
+]);
